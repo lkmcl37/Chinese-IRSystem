963488830a6fafa2a6be3c0634b3e8a9335322d1
lkmcl37 <lkmcl37@hotmail.com>
2016-01-08 21:12:56 +0800

    Update Test.java

--- Information-Retrieval/src/Test.java
+++ Information-Retrieval/src/Test.java
@@ -10,14 +10,8 @@ public class Test {
 		myIRSystem.createIndex();	// must create before search
 		
 		
-		System.out.println("ËÑË÷ ½ÉÄÉ");
-		myIRSystem.search("½ÉÄÉ");
+		System.out.println("search æ±½è½¦");
+		myIRSystem.search("æ±½è½¦");
 		System.out.println();
-		System.out.println("ËÑË÷ Ê¯ÓÍ");
-		myIRSystem.search("Ê¯ÓÍ");
-		System.out.println();
-		System.out.println("ËÑË÷ Æû³µ");
-		myIRSystem.search("Æû³µ");
-
 	}
 }
